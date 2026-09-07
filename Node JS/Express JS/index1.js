66ff521b093242e1d251b487f80edefcbab82ce9
Aditya <chauhanaditya1112006@gmail.com>
2026-09-07 20:01:59 +0530

    Path parameters and query results

--- Node JS/Express JS/index1.js
+++ Node JS/Express JS/index1.js
@@ -17,3 +17,13 @@ app.get('/:username/:id', (req,res) => {
     let {username , id} = req.params;
     res.send(`Hello this path is belongs to @${username} having id ${id}`);
 })
+
+// query results
+
+app.get('/search', (req,res)=>{
+    let {q} = req.query;
+    if(!q){
+        res.send("Nothing searched");
+    }
+    res.send(`Query for ${q}`);
+})
