c205e517c72708a8db4b10143910f5df62dce76c
Aditya <chauhanaditya1112006@gmail.com>
2026-09-07 19:31:52 +0530

    Handling request, sending response and routing

--- Node JS/Express JS/index.js
+++ Node JS/Express JS/index.js
@@ -4,11 +4,28 @@ const app = express();
 let port = 3000;
 
 app.listen(port, ()=>{
-    console.log(`Server is running at port ${port}`);
+    console.log(`Server is running at port ${port}`); // Handling request
 })
 
 app.use((req, res) =>{
     console.log("request recieved");
     let heroes = "<h1>Avengers</h1><ul><li>Iron Man</li><li>Thor</li><li>Captain America</li><li>Hulk</li><li>Black Widow</li><li>Hawkeye</li></ul>";
-    res.send(heroes);
+    res.send(heroes); // Sending a response
+})
+
+app.get('/', (req,res) => {
+    res.send("Request for root path");
+})
+app.post('', (req,res) => {
+    res.send("Request for post root path");
+})
+app.get('/ironman', (req,res) => {
+    res.send("Request for Iron Man path");
+})
+app.get('/spidey', (req,res) => {
+    res.send("Request for Spidey path");
+})
+app.get("/*splat", (req, res) => { // wildcard that do not throw error whereas display message instead of that
+    let message = "<h1>No such path defined</h1>";
+    res.send(message);
 })
